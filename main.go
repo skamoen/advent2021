7c53fc9f061a62965f059bb31c4918d4261e545e
Sille Kamoen <github@sille.me>
2021-12-01 20:33:50 +0100

    Day 1 part 2

--- main.go
+++ main.go
@@ -20,10 +20,12 @@ func day1() {
 	defer file.Close()
 
 	deeper := 0
+	var measurements []int64
 
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
 	prevReading, err := strconv.ParseInt(scanner.Text(), 10, 64)
+	measurements = append(measurements, prevReading)
 	for scanner.Scan() {
 		reading, err := strconv.ParseInt(scanner.Text(), 10, 64)
 		if err != nil {
@@ -32,6 +34,8 @@ func day1() {
 		if reading > prevReading {
 			deeper++
 		}
+
+		measurements = append(measurements, reading)
 		prevReading = reading
 	}
 
@@ -39,5 +43,22 @@ func day1() {
 		log.Fatal(err)
 	}
 
+	deeperWindows := 0
+	var prevSum int64
+	for i := 0; i <= len(measurements)-3; i++ {
+		currentWindow := measurements[i : i+3]
+		var currentSum int64 = 0
+		for _, m := range currentWindow {
+			currentSum = currentSum + m
+		}
+		if prevSum != 0 {
+			if currentSum > prevSum {
+				deeperWindows++
+			}
+		}
+		prevSum = currentSum
+	}
+
 	fmt.Println("Readings deeper: ", deeper)
+	fmt.Println("Readings deeper windows: ", deeperWindows)
 }
