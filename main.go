122645fc7c7bd392b1595c554b24916fd4939dd4
Sille Kamoen <github@sille.me>
2021-12-09 08:38:19 +0100

    Day 9 part 2

--- main.go
+++ main.go
@@ -46,6 +46,7 @@ func day9() {
 	}
 
 	riskSum := 0
+	var sizes []int
 	for i := range heights {
 		for j := range heights[i] {
 			currentHeight := heights[i][j]
@@ -75,13 +76,72 @@ func day9() {
 				}
 			}
 			riskSum += currentHeight + 1
+			b := &basin{start: []int{i, j}}
+			size := b.findSize(heights)
+			sizes = append(sizes, size)
 		}
 	}
 
-	fmt.Println("Riskfactor", riskSum)
+	sort.Ints(sizes)
+	ints := sizes[len(sizes)-3:]
+	fmt.Println("Riskfactor", riskSum, "Basin size", ints[0]*ints[1]*ints[2])
 
 	diff := time.Now().Sub(start)
 	fmt.Println("Took", diff.Microseconds(), "microseconds")
+
+}
+
+func (b *basin) findSize(heights [][]int) int {
+	b.marker = make([][]bool, len(heights))
+	for i := 0; i < len(heights); i++ {
+		b.marker[i] = make([]bool, len(heights))
+	}
+
+	// Check
+	b.checkPoint(b.start[0], b.start[1], heights)
+
+	return b.size
+}
+
+func (b *basin) checkPoint(i, j int, heights [][]int) {
+
+	if b.marker[i][j] == false {
+		b.size++
+		b.marker[i][j] = true
+	} else {
+		return
+	}
+
+	// Check left
+	if i > 0 {
+		if heights[i-1][j] != 9 {
+			b.checkPoint(i-1, j, heights)
+		}
+	}
+	// Check up
+	if j > 0 {
+		if heights[i][j-1] != 9 {
+			b.checkPoint(i, j-1, heights)
+		}
+	}
+	// Check right
+	if i < len(heights)-1 {
+		if heights[i+1][j] != 9 {
+			b.checkPoint(i+1, j, heights)
+		}
+	}
+	// Check bottom
+	if j < len(heights[i])-1 {
+		if heights[i][j+1] != 9 {
+			b.checkPoint(i, j+1, heights)
+		}
+	}
+}
+
+type basin struct {
+	start  []int
+	marker [][]bool // Mark X, Y as included
+	size   int
 }
 
 func minInt(a, b int) int {
