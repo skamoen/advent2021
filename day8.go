278796655d1d1acb758c8e3a9fb9b93ca8077675
Sille Kamoen <github@sille.me>
2021-12-09 08:08:43 +0100

    Day 9 part 1

--- day8.go
+++ day8.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"log"
+	"math"
+)
+
 func arrayContainsAll(a []string, b []string) bool {
 	for _, s := range b {
 		if !arrayContains(a, s) {
@@ -28,4 +33,92 @@ func arrayContains(s []string, str string) bool {
 		}
 	}
 	return false
+}
+
+func (d *display) value() int {
+	if len(d.knownSignals) != 10 {
+		log.Println("Not solved yet")
+		return 0
+	}
+
+	value := 0
+	for i, o := range d.output {
+		for v, s := range d.knownSignals {
+			if arrayContainsExact(o, s) {
+				value += int(math.Pow10(3-i)) * v
+			}
+		}
+	}
+	return value
+}
+
+func (d *display) solve() {
+	for i := 0; len(d.knownSignals) < 10; i++ {
+		for i, wire := range d.wires {
+			if d.solved[i] {
+				continue
+			}
+			if len(wire) == 5 {
+				if arrayContainsAll(wire, d.knownSignals[1]) {
+					// It's 3
+					d.knownSignals[3] = wire
+					d.solved[i] = true
+				} else if _, ok := d.knownSignals[6]; ok {
+					if arrayContainsAll(d.knownSignals[6], wire) {
+						// It's 5
+						d.knownSignals[5] = wire
+						d.solved[i] = true
+					} else {
+						// It's 2
+						d.knownSignals[2] = wire
+						d.solved[i] = true
+					}
+				}
+			} else if len(wire) == 6 {
+				// 6 or 9
+				if _, ok := d.knownSignals[3]; ok {
+					if arrayContainsAll(wire, d.knownSignals[3]) {
+						// It's 9
+						d.knownSignals[9] = wire
+						d.solved[i] = true
+					} else {
+						if _, ok := d.knownSignals[7]; ok {
+							if arrayContainsAll(wire, d.knownSignals[7]) {
+								// It's 0
+								d.knownSignals[0] = wire
+								d.solved[i] = true
+							} else {
+								// it's 6
+								d.knownSignals[6] = wire
+								d.solved[i] = true
+							}
+						}
+					}
+				}
+			}
+			if len(d.knownSignals) == 10 {
+				return
+			}
+		}
+	}
+}
+
+func (d *display) setUniqeMappings(wire []string) {
+	switch len(wire) {
+	case 2:
+		d.knownSignals[1] = wire
+	case 4:
+		d.knownSignals[4] = wire
+	case 3:
+		d.knownSignals[7] = wire
+	case 7:
+		d.knownSignals[8] = wire
+	}
+}
+
+type display struct {
+	wires        [][]string
+	output       [][]string
+	knownSignals map[int][]string
+	solved       map[int]bool
 }
--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"sort"
 	"strconv"
@@ -20,7 +19,77 @@ func main() {
 	//day5()
 	//day6()
 	//day7()
-	day8()
+	//day8()
+	day9()
+}
+
+func day9() {
+	start := time.Now()
+	file, err := os.Open("./9/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var heights [][]int
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		var row = make([]int, len(line))
+		for i := range line {
+			c, _ := strconv.Atoi(string(line[i]))
+			row[i] = c
+		}
+		heights = append(heights, row)
+	}
+
+	riskSum := 0
+	for i := range heights {
+		for j := range heights[i] {
+			currentHeight := heights[i][j]
+
+			// Check up
+			if j > 0 {
+				if heights[i][j-1] == currentHeight || minInt(currentHeight, heights[i][j-1]) != currentHeight {
+					continue
+				}
+			}
+			// Check right
+			if i < len(heights)-1 {
+				if heights[i+1][j] == currentHeight || minInt(currentHeight, heights[i+1][j]) != currentHeight {
+					continue
+				}
+			}
+			// Check bottom
+			if j < len(heights[i])-1 {
+				if heights[i][j+1] == currentHeight || minInt(currentHeight, heights[i][j+1]) != currentHeight {
+					continue
+				}
+			}
+			// Check left
+			if i > 0 {
+				if heights[i-1][j] == currentHeight || minInt(currentHeight, heights[i-1][j]) != currentHeight {
+					continue
+				}
+			}
+			riskSum += currentHeight + 1
+		}
+	}
+
+	fmt.Println("Riskfactor", riskSum)
+
+	diff := time.Now().Sub(start)
+	fmt.Println("Took", diff.Microseconds(), "microseconds")
+}
+
+func minInt(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
 }
 
 func day8() {
@@ -62,94 +131,6 @@ func day8() {
 	diff := time.Now().Sub(start)
 	fmt.Println("Took", diff.Microseconds(), "microseconds")
 
-}
-
-func (d *display) value() int {
-	if len(d.knownSignals) != 10 {
-		log.Println("Not solved yet")
-		return 0
-	}
-
-	value := 0
-	for i, o := range d.output {
-		for v, s := range d.knownSignals {
-			if arrayContainsExact(o, s) {
-				value += int(math.Pow10(3-i)) * v
-			}
-		}
-	}
-	return value
-}
-
-func (d *display) solve() {
-	for i := 0; len(d.knownSignals) < 10; i++ {
-		for i, wire := range d.wires {
-			if d.solved[i] {
-				continue
-			}
-			if len(wire) == 5 {
-				if arrayContainsAll(wire, d.knownSignals[1]) {
-					// It's 3
-					d.knownSignals[3] = wire
-					d.solved[i] = true
-				} else if _, ok := d.knownSignals[6]; ok {
-					if arrayContainsAll(d.knownSignals[6], wire) {
-						// It's 5
-						d.knownSignals[5] = wire
-						d.solved[i] = true
-					} else {
-						// It's 2
-						d.knownSignals[2] = wire
-						d.solved[i] = true
-					}
-				}
-			} else if len(wire) == 6 {
-				// 6 or 9
-				if _, ok := d.knownSignals[3]; ok {
-					if arrayContainsAll(wire, d.knownSignals[3]) {
-						// It's 9
-						d.knownSignals[9] = wire
-						d.solved[i] = true
-					} else {
-						if _, ok := d.knownSignals[7]; ok {
-							if arrayContainsAll(wire, d.knownSignals[7]) {
-								// It's 0
-								d.knownSignals[0] = wire
-								d.solved[i] = true
-							} else {
-								// it's 6
-								d.knownSignals[6] = wire
-								d.solved[i] = true
-							}
-						}
-					}
-				}
-			}
-			if len(d.knownSignals) == 10 {
-				return
-			}
-		}
-	}
-}
-
-func (d *display) setUniqeMappings(wire []string) {
-	switch len(wire) {
-	case 2:
-		d.knownSignals[1] = wire
-	case 4:
-		d.knownSignals[4] = wire
-	case 3:
-		d.knownSignals[7] = wire
-	case 7:
-		d.knownSignals[8] = wire
-	}
-}
-
-type display struct {
-	wires        [][]string
-	output       [][]string
-	knownSignals map[int][]string
-	solved       map[int]bool
 }
 
 func day7() {
