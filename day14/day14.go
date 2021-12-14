5a8a8b7a73083b3e4b61618094df13092a91c629
Sille Kamoen <github@sille.me>
2021-12-14 09:02:19 +0100

    Day 14 part 2

--- day14/day14.go
+++ day14/day14.go
@@ -30,37 +30,68 @@ func (*d) Run() (int, int) {
 	polymer := scanner.Text()
 	scanner.Scan()
 
-	var insert = make(map[string]string, 0)
+	var pairs = make(map[string]*pair, 1)
+	var counter = make(map[string]int, 1)
 
 	for scanner.Scan() {
 		line := scanner.Text()
 		split := strings.Split(line, " -> ")
-		insert[split[0]] = split[1]
-	}
-
-	newPolymer := polymer
-	for n := 0; n < 10; n++ {
-		for i := 0; i < len(polymer)-1; i++ {
-			pair := polymer[i : i+2]
-			lookup := insert[pair]
-			newPolymer = newPolymer[:i+i+1] + lookup + newPolymer[i+i+1:]
+		pairs[split[0]] = &pair{
+			letters: [2]string{string(split[0][0]), string(split[0][1])},
+			insert:  split[1],
+			count:   0,
 		}
-		polymer = newPolymer
 	}
 
-	min, max := minMax(countCharacters(polymer))
+	for i := 0; i < len(polymer); i++ {
+		if i < len(polymer)-1 {
+			currentPair := polymer[i : i+2]
+			pairs[currentPair].count++
+		}
+		counter[string(polymer[i])]++
+	}
 
-	return max - min, 0
+	partOne := 0
+	for n := 1; n <= 40; n++ {
+		var currentKeys = make([]string, 0, len(pairs))
+		for s := range pairs {
+			if pairs[s].count > 0 {
+				currentKeys = append(currentKeys, s)
+			}
+		}
+
+		for _, k := range currentKeys {
+			currentPair := pairs[k]
+
+			counter[currentPair.insert] += currentPair.count
+
+			newPairA := currentPair.letters[0] + currentPair.insert
+			pairs[newPairA].add += currentPair.count
+
+			newPairB := currentPair.insert + currentPair.letters[1]
+			pairs[newPairB].add += currentPair.count
+
+			currentPair.count = 0
+		}
+		for _, p := range pairs {
+			p.count += p.add
+			p.add = 0
+		}
+		if n == 10 {
+			min, max := minMax(counter)
+			partOne = max - min
+		}
+	}
+
+	min, max := minMax(counter)
+	return partOne, max - min
 }
 
-func countCharacters(s string) map[string]int {
-	var counter = make(map[string]int, 1)
-
-	for i := range s {
-		counter[string(s[i])]++
-	}
-
-	return counter
+type pair struct {
+	letters [2]string
+	insert  string
+	count   int
+	add     int
 }
 
 func minMax(m map[string]int) (int, int) {
